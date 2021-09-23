ca385e016f450b82299349830d61367c7a3a058f
Aayush Bajaj <aayushbajaj505@gmail.com>
2021-09-24 01:39:48 +0530

    m

--- Arrays/2D-Arrays/challenges/matrix_multiplication.cpp
+++ Arrays/2D-Arrays/challenges/matrix_multiplication.cpp
@@ -3,4 +3,25 @@ using namespace std;
 
 int main()
 {
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    int a1[n1][n2]; //arr1
+    int a2[n2][n3]; //arr2
+    int ans[n1][n3];
+    // input of arr1
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            cin >> a1[i][j];
+        }
+    }
+    // input of arr2
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cin >> a2[i][j];
+        }
+    }
 }
