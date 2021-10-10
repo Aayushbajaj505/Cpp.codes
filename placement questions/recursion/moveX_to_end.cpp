b7a365801745245e2b8be6b8344f1dfbae42ca8e
Aayush Bajaj <aayushbajaj505@gmail.com>
2021-10-10 21:44:25 +0530

    lo

---
+++ placement questions/recursion/moveX_to_end.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+string move(string s)
+{
+    if (s.length() == 0)
+    {
+        return "";
+    }
+    char ch = s[0];
+    string ans = move(s.substr(1));
+    if (ch == 'x')
+    {
+        return ans + ch;
+    }
+    return ch + ans;
+}
+int main()
+{
+    cout << move("axbxxcy");
+}
