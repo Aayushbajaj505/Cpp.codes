105d7ea719b72bc43d23ec4b567df95a6ba5d113
Aayush Bajaj <aayushbajaj505@gmail.com>
2021-10-27 22:13:17 +0530

    dfg

---
+++ placement questions/recursion/backtracking.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+// BackTracking is an algorithmic technique for solving
+// recursive problems by trying top build every
+// possible solution incrementally, and removing
+// those solutions tha tfail to satisfy the constraints of the problem\
+// at any point of time.
+int main()
+{
+}
