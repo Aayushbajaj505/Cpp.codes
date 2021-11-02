fe4d1eb700d6e73575308c1ef08ca3f35fd84e33
Aayush Bajaj <aayushbajaj505@gmail.com>
2021-11-02 14:21:08 +0530

    6

--- Arrays/Sorting/merge_sort.cpp
+++ Arrays/Sorting/merge_sort.cpp
@@ -1,78 +1,87 @@
-#include <bits/stdc++.h>
-#include <algorithm>
-#include <string>
-#include <climits>
+#include <iostream>
 using namespace std;
-void merge(int arr[], int l, int mid, int r)
+void swapping(int &a, int &b)
+{ //swap the content of a and b
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void display(int *array, int size)
 {
-    int n1 = mid - l + 1;
-    int n2 = r - mid;
-    // temp arrays
-    int a[n1];
-    int b[n2];
-    // the main array at this point contains
-    // two sorted arrays but needs to be merged
-    // store both arrays in temp array because checking and
-    // opertaion cannot be done on the same array
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < size; i++)
+        cout << array[i] << " ";
+    cout << endl;
+}
+void merge(int *array, int l, int m, int r)
+{
+    int i, j, k, nl, nr;
+    //size of left and right sub-arrays
+    nl = m - l + 1;
+    nr = r - m;
+    int larr[nl], rarr[nr];
+    //fill left and right sub-arrays
+    for (i = 0; i < nl; i++)
+        larr[i] = array[l + i];
+    for (j = 0; j < nr; j++)
+        rarr[j] = array[m + 1 + j];
+    i = 0;
+    j = 0;
+    k = l;
+    //marge temp arrays to real array
+    while (i < nl && j < nr)
     {
-        a[i] = arr[l + i];
-    }
-    for (int i = 0; i < n2; i++)
-    {
-        b[i] = arr[mid + 1 + i];
-    }
-    int i = 0;
-    int j = 0;
-    int k = l;
-    while (i < n1 && j < n2)
-    {
-        if (a[i] < b[j])
+        if (larr[i] <= rarr[j])
         {
-            arr[k] = a[i];
-            k++;
+            array[k] = larr[i];
             i++;
         }
         else
         {
-            arr[k] = b[j];
-            k++;
+            array[k] = rarr[j];
             j++;
         }
-        //edge case if i does not reach n1
-        while (i < n1)
-        {
-            arr[k] = a[i];
-            k++;
-            i++;
-        }
-        //edge case if j does not reach n2
-        while (j < n2)
-        {
-            arr[k] = b[j];
-            k++;
-            j++;
-        }
+        k++;
+    }
+    while (i < nl)
+    { //extra element in left array
+        array[k] = larr[i];
+        i++;
+        k++;
+    }
+    while (j < nr)
+    { //extra element in right array
+        array[k] = rarr[j];
+        j++;
+        k++;
     }
 }
-void mergeSort(int arr[], int l, int r)
+void mergeSort(int *array, int l, int r)
 {
+    int m;
     if (l < r)
     {
-        int mid = (l + r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid, r);
-
-        merge(arr, l, mid, r);
+        int m = l + (r - l) / 2;
+        // Sort first and second arrays
+        mergeSort(array, l, m);
+        mergeSort(array, m + 1, r);
+        merge(array, l, m, r);
     }
 }
 int main()
 {
-    int arr[] = {5, 4, 3, 2, 1};
-    mergeSort(arr, 0, 4);
-    for (int i = 0; i < 5; i++)
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n]; //create an array with given number of elements
+    cout << "Enter elements:" << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        cin >> arr[i];
     }
-    cout << endl;
+    cout << "Array before Sorting: ";
+    display(arr, n);
+    mergeSort(arr, 0, n - 1); //(n-1) for last index
+    cout << "Array after Sorting: ";
+    display(arr, n);
 }
