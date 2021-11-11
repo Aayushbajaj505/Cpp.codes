0a281f072b4f0d0a5dcef2fe0a113f9b73d3b29a
Aayush Bajaj <aayushbajaj505@gmail.com>
2021-11-11 12:49:38 +0530

    d

--- Trees/tree.cpp
+++ Trees/tree.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 #include <vector>
 #include <stack>
+#include <queue>
 // #include <TreeNode.h>
 using namespace std;
 template <typename T>
@@ -53,6 +54,47 @@ TreeNode<int> *takeInput()
     }
     return root;
 }
+
+TreeNode<int> *takeInputLevelwise()
+{
+
+    int rootdata;
+    // first get the root node
+    cout << "enter root data" << endl;
+    cin >> rootdata;
+    // make a dynamiclly allocated node
+    TreeNode<int> *root = new TreeNode<int>(rootdata);
+    // define a queue
+    queue<TreeNode<int> *> pendingNodes;
+    // push the root node to the queue
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        // grab the first node from teh queue
+        TreeNode<int> *front = pendingNodes.front();
+        // pop the node
+        pendingNodes.pop();
+        // ask for number of children
+        cout << "enter Number oif children of " << front->data << endl;
+        int numChild;
+        cin >> numChild;
+        // get all the children
+        for (int i = 0; i < numChild; i++)
+        {
+
+            cout << "Enter " << i << " th child of " << front->data << endl;
+            int childData;
+            cin >> childData;
+            // make a new node for each child
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            // push the children to the parent vector
+            front->children.push_back(child);
+            // also push the children to the pending nodes queue
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
 int main()
 {
     // TreeNode<int> *root = new TreeNode<int>(1);
@@ -61,6 +103,6 @@ int main()
     // root->children.push_back(node1);
     // root->children.push_back(node2);
     // printTree(root);
-    TreeNode<int> *root = takeInput();
+    TreeNode<int> *root = takeInputLevelwise();
     printTree(root);
 }
