4dfd78522d032aea13298141b3bd712bc72657de
Aayush Bajaj <aayushbajaj505@gmail.com>
2022-11-05 15:40:09 +0530

    uy

--- Dalhousie Codes/find_name.cpp
+++ Dalhousie Codes/find_name.cpp
@@ -7,17 +7,28 @@
 #include <queue>
 #include <unordered_map>
 using namespace std;
-bool binary_search(vector<string> s, string key)
+string binary_search(vector<string> s, string key)
 {
-    int st=0;
-    int e=s.size();
-    while(st<e){
-        int m=st+(e-1)/2;
-        if(s[m].compare(key))
-            return true;
-        else{}
+    int st = 0;
+    int e = s.size() - 1;
+    while (st <= e)
+    {
+        int m = (st + (e - 1)) / 2;
+        // cout << s[m];
+        if (key == s[m])
+        {
+            return "true";
+        }
+        if (key > s[m])
+            st = m + 1;
+        if (key < s[m])
+            e = m - 1;
     }
+    return "false";
 }
 int main()
 {
+    vector<string> a = {"Aayush", "Ajay", "rajni", "Stool", "smol", "boops"};
+    cout << binary_search(a, "abc");
+    return 0;
 }
